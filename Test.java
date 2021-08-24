2cabae6cf8d2db984f74a96d41b14548c1463df1
lanjie1 <89454128+lanjie1@users.noreply.github.com>
2021-08-24 18:35:11 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
 
-pubic void main(String [] args){
-System.out.println("HELLE WORLD!!");
-}
+	public static void main(String [] args){
+		System.out.println("HELLO WORLD!!");
+	}
 }
